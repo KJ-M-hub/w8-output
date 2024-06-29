bc54de6416b000175810c7c49aec798663a77f6d
KJ-M <rfb0920@gmail.com>
2024-06-29 10:58:56 +0900

    Revert "目のアイコンを追加し、grid-item内に数字を追加しました。"
    This reverts commit c80c72299a5953e53da779fda3112311ba1a9ed1.

--- html-css/script.js
+++ html-css/script.js
@@ -1,17 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
     const emailInput = document.getElementById('email');
     const passwordInput = document.getElementById('password');
     const togglePassword = document.getElementById('toggle-password');
     const form = document.querySelector('.form');
-
-
-    // フォーム送信時にemailとpasswordをlocalStorageに保存
-    form.addEventListener('submit', (e) => {
-        e.preventDefault(); // フォームのデフォルト動作を防ぐ
-        localStorage.setItem('email', emailInput.value);
-        localStorage.setItem('password', passwordInput.value);
-        form.submit(); // フォームを送信
-    });
 
     // ページ読み込み時にlocalStorageからデータを取得して自動入力
     if (localStorage.getItem('email')) {
@@ -21,8 +12,16 @@ document.addEventListener('DOMContentLoaded', () => {
         passwordInput.value = localStorage.getItem('password');
     }
 
-    // パスワード表示アイコンをクリックするとパスワードが見えるイベント
-    togglePassword.addEventListener('click', () => {
+    // フォーム送信時にemailとpasswordをlocalStorageに保存
+    form.addEventListener('submit', function(e) {
+        e.preventDefault(); // フォームのデフォルト動作を防ぐ
+        localStorage.setItem('email', emailInput.value);
+        localStorage.setItem('password', passwordInput.value);
+        form.submit(); // フォームを送信
+    });
+
+    // パスワード表示アイコンのクリックイベントを監視
+    togglePassword.addEventListener('click', function() {
         if (passwordInput.type === 'password') {
             passwordInput.type = 'text';
         } else {
