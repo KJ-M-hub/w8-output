c80c72299a5953e53da779fda3112311ba1a9ed1
KJ-M <rfb0920@gmail.com>
2024-06-29 10:58:37 +0900

    目のアイコンを追加し、grid-item内に数字を追加しました。

--- html-css/script.js
+++ html-css/script.js
@@ -1,8 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
     const emailInput = document.getElementById('email');
     const passwordInput = document.getElementById('password');
     const togglePassword = document.getElementById('toggle-password');
     const form = document.querySelector('.form');
+
+
+    // フォーム送信時にemailとpasswordをlocalStorageに保存
+    form.addEventListener('submit', (e) => {
+        e.preventDefault(); // フォームのデフォルト動作を防ぐ
+        localStorage.setItem('email', emailInput.value);
+        localStorage.setItem('password', passwordInput.value);
+        form.submit(); // フォームを送信
+    });
 
     // ページ読み込み時にlocalStorageからデータを取得して自動入力
     if (localStorage.getItem('email')) {
@@ -12,16 +21,8 @@ document.addEventListener('DOMContentLoaded', function() {
         passwordInput.value = localStorage.getItem('password');
     }
 
-    // フォーム送信時にemailとpasswordをlocalStorageに保存
-    form.addEventListener('submit', function(e) {
-        e.preventDefault(); // フォームのデフォルト動作を防ぐ
-        localStorage.setItem('email', emailInput.value);
-        localStorage.setItem('password', passwordInput.value);
-        form.submit(); // フォームを送信
-    });
-
-    // パスワード表示アイコンのクリックイベントを監視
-    togglePassword.addEventListener('click', function() {
+    // パスワード表示アイコンをクリックするとパスワードが見えるイベント
+    togglePassword.addEventListener('click', () => {
         if (passwordInput.type === 'password') {
             passwordInput.type = 'text';
         } else {
